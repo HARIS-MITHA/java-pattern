29228e76d3adb049c0af1198ef5cd1099671d713
unknown <harismitha1308@gmail.com>
2026-02-26 20:57:06 +0530

    added

---
+++ diamond.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class diamond{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for(int j = 1; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+        for(int i = 1; i <= n - 1; i++) {
+            for(int j = 1; j <= i; j++)
+                System.out.print(" ");
+            for(int j = 1; j <= n - i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
